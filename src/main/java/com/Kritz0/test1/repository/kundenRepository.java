eccf5b5406ea3a2fec36b3a2695f99c343165e81
Kritz0 <simon.kritsotakis@live.at>
2020-11-18 15:46:04 +0100

    test upload1

---
+++ src/main/java/com/Kritz0/test1/repository/kundenRepository.java
@@ -0,0 +1,9 @@
+package com.Kritz0.test1.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Kritz0.test1.pkg.kunde;
+
+public interface kundenRepository extends JpaRepository<kunde, Integer>{
+
+}
